d46b02f8a3eb5ea048152e2b79ab6e1209cbfec5
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2019-02-23 10:50:53 +1300

    Empty template for compute device.

---
+++ source/Vizor/ComputeDevice.cpp
@@ -0,0 +1,20 @@
+//
+//  ComputeDevice.cpp
+//  This file is part of the "Vizor" project and released under the MIT License.
+//
+//  Created by Samuel Williams on 18/2/2019.
+//  Copyright, 2019, by Samuel Williams. All rights reserved.
+//
+
+#include "ComputeDevice.hpp"
+
+namespace Vizor
+{
+	ComputeDevice::ComputeDevice()
+	{
+	}
+	
+	ComputeDevice::~ComputeDevice()
+	{
+	}
+}
---
+++ source/Vizor/ComputeDevice.hpp
@@ -0,0 +1,22 @@
+//
+//  ComputeDevice.hpp
+//  This file is part of the "Vizor" project and released under the MIT License.
+//
+//  Created by Samuel Williams on 18/2/2019.
+//  Copyright, 2019, by Samuel Williams. All rights reserved.
+//
+
+#pragma once
+
+namespace Vizor
+{
+	class ComputeDevice
+	{
+	public:
+		ComputeDevice();
+		virtual ~ComputeDevice();
+		
+	private:
+		
+	};
+}
